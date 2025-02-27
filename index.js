a39fefb1f6fd6999d0905f0cd3ffedd7ce113727
greatstackuser <user.greatstack@gmail.com>
2025-02-27 14:17:43 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const { delay } = require("q");
+const { string } = require("yargs");
+
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector ('.navbar');
+
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+let sections = document.querySelectorAll('section');
+
+let navLink = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach (sec => {
+        let top = window.scrollY; 
+        let offset = sec.offsetTop - 150 ;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+
+        if (top >= offset && top < offset + height ) {
+            navLink.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href* = ' + id + ']').classList.add('active');
+
+            });
+        };
+    });
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+ScrollReveal ({
+    reset : true,
+    distance: '80px',
+    duration : 2000,
+    delay : 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin : 'top'} );
+
+ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact form', { origin : 'bottom'} );
+
+ScrollReveal().reveal('.home-content h1, .about-img, .about-img', { origin: 'left'});
+
+ScrollReveal().reveal('.home-content p, .about-img, .about-img', { origin: 'right'});
+
+
+const typed = new Typed ('.multipe-text', {
+
+    strings : ['Frontend Developer', 'UX/UI Developer', 'Blogger'],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+
+});
